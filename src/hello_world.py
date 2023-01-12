31249278658c3db10822e28ff5a02e769192b83d
Saikat Bandyopadhyay <saikat.10.05.95@gmail.com>
2023-01-12 11:26:34 -0500

    initial project structure

---
+++ src/hello_world.py
@@ -0,0 +1,10 @@
+# ---- start of a file
+# a basic hello world python script
+
+# please change the user name
+user = "Saikat"
+
+# print a basic message
+print(f"Hello world!\n-creator {user}")
+
+# --- end of file
